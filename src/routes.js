2dee2b2f8fe5b8cf3291d015b3c7fbf59be16b4a
Aimable <niyoai@yahoo.fr>
2018-02-17 18:19:53 +0000

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+
+
+import App from './components/posts_show';
+import PostIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+
+export default(
+    <Route path="/" component={App}>
+        <IndexRoute component={PostIndex}/>
+        <Route path="posts/new" component={PostsNew}/>
+
+    </Route>
+);
+
+
+
+
